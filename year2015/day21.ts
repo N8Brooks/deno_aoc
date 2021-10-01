367cff5ce84328899bf0b3bdf138de44a8712583
Nathan Brooks <natebrooks004@gmail.com>
2021-09-30 18:48:45 -0700

    2015/21

---
+++ year2015/day21.ts
@@ -0,0 +1,76 @@
+import { combinations } from "https://deno.land/x/combinatorics/combinations.ts";
+import { product } from "https://deno.land/x/combinatorics/product.ts";
+
+type Items = [number, number, number];
+
+const WEAPS: Items[] = [
+  [8, 4, 0],
+  [10, 5, 0],
+  [25, 6, 0],
+  [40, 7, 0],
+  [74, 8, 0],
+];
+
+const ARMOR: Items[] = [
+  [13, 0, 1],
+  [31, 0, 2],
+  [53, 0, 3],
+  [75, 0, 4],
+  [102, 0, 5],
+];
+
+const RINGS: Items[] = [
+  [25, 1, 0],
+  [50, 2, 0],
+  [100, 3, 0],
+  [20, 0, 1],
+  [40, 0, 2],
+  [80, 0, 3],
+];
+
+export function part1(text: string, pHealth: number) {
+  return simulate(text, pHealth, true);
+}
+
+export function part2(text: string, pHealth: number) {
+  return simulate(text, pHealth, false);
+}
+
+function simulate(text: string, pHealth: number, win: boolean) {
+  const [bHealth, bDamage, bArmor] = [...text.matchAll(/\d+/g)].map((d) => +d);
+
+  const weaps: Items[] = WEAPS;
+  const armor: Items[] = [[0, 0, 0], ...ARMOR];
+  const rings: Items[] = [
+    [0, 0, 0],
+    ...RINGS,
+    ...[...combinations(2, RINGS)].map(stats),
+  ];
+
+  const equipment = [...product(1, weaps, armor, rings)].map(stats);
+  const costs = equipment
+    .filter((items) => battle(items) === win)
+    .map(([cost]) => cost);
+
+  return win ? Math.min(...costs) : Math.max(...costs);
+
+  function battle([, pDamage, pArmor]: Items): boolean {
+    const bTurns = turns(pHealth, bDamage - pArmor);
+    const pTurns = turns(bHealth, pDamage - bArmor);
+    return pTurns <= bTurns;
+  }
+}
+
+function stats(items: Items[]): Items {
+  let cost = 0, damage = 0, armor = 0;
+  for (const [c, d, a] of items) {
+    cost += c;
+    damage += d;
+    armor += a;
+  }
+  return [cost, damage, armor];
+}
+
+function turns(a: number, b: number): number {
+  return b > 0 ? Math.ceil(a / b) : Infinity;
+}
---
+++ year2015/day21_test.ts
@@ -0,0 +1,20 @@
+import { assertEquals } from "https://deno.land/std@0.109.0/testing/asserts.ts";
+import { part1, part2 } from "./day21.ts";
+
+const text = await Deno.readTextFile("year2015/day21.txt");
+
+Deno.test("part1(example)", () => {
+  assertEquals(part1("8 5 5", 12), 65);
+});
+
+Deno.test("part1(text)", () => {
+  assertEquals(part1(text, 100), 111);
+});
+
+Deno.test("part2(example)", () => {
+  assertEquals(part2("6 7 2", 8), 88);
+});
+
+Deno.test("part2(text)", () => {
+  assertEquals(part2(text, 100), 188);
+});
