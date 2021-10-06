9e88c80c6a3092447f3cb9d32818ad296b1ee268
Nathan Brooks <natebrooks004@gmail.com>
2021-10-06 09:02:44 -0700

    year2015/day10

--- year2015/day10.ts
+++ year2015/day10.ts
@@ -1,3 +1,19 @@
+export function part1(data: string): number {
+  let result = data.trimEnd().split("");
+  for (let i = 0; i < 40; i++) {
+    result = lookSay(result);
+  }
+  return result.length;
+}
+
+export function part2(data: string): number {
+  let result = data.trimEnd().split("");
+  for (let i = 0; i < 50; i++) {
+    result = lookSay(result);
+  }
+  return result.length;
+}
+
 export function lookSay(values: string[] | string): string[] {
   const result: string[] = [];
   let previousValue = "";
@@ -14,20 +30,4 @@ export function lookSay(values: string[] | string): string[] {
   }
   result.push(length + "", previousValue);
   return result;
-}
-
-export function part1(data: string): number {
-  let result = data.trimEnd().split("");
-  for (let i = 0; i < 40; i++) {
-    result = lookSay(result);
-  }
-  return result.length;
-}
-
-export function part2(data: string): number {
-  let result = data.trimEnd().split("");
-  for (let i = 0; i < 50; i++) {
-    result = lookSay(result);
-  }
-  return result.length;
 }
