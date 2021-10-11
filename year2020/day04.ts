8e27383b2a532691e3cc0c350c44f39af513e185
Nathan Brooks <natebrooks004@gmail.com>
2021-10-10 21:15:01 -0700

    year2020/day04

---
+++ year2020/day04.ts
@@ -0,0 +1,82 @@
+const pattern = /([a-z]{3}):([a-z0-9#]+)/g;
+
+const passportFields = [
+  "byr",
+  "iyr",
+  "eyr",
+  "hgt",
+  "hcl",
+  "ecl",
+  "pid",
+];
+
+const eyeColors = {
+  amb: true,
+  blu: true,
+  brn: true,
+  gry: true,
+  grn: true,
+  hzl: true,
+  oth: true,
+};
+
+export function part1(data: string): number {
+  let valid = 0;
+  for (const passportData of data.split("\n\n")) {
+    const passport = getPassportObject(passportData);
+    if (passportFields.every((field) => field in passport)) {
+      valid++;
+    }
+  }
+  return valid;
+}
+
+export function part2(data: string): number {
+  let valid = 0;
+  for (const passportData of data.split("\n\n")) {
+    const {
+      byr = "",
+      iyr = "",
+      eyr = "",
+      hgt = "",
+      hcl = "",
+      ecl = "",
+      pid = "",
+    } = getPassportObject(passportData);
+
+    if (+byr < 1920 || 2002 < +byr) {
+      continue;
+    } else if (+iyr < 2010 || 2020 < +iyr) {
+      continue;
+    } else if (+eyr < 2020 || 2030 < +eyr) {
+      continue;
+    } else if (!/^#[a-f0-9]{6}$/.test(hcl)) {
+      continue;
+    } else if (!(ecl in eyeColors)) {
+      continue;
+    } else if (!/^\d{9}$/.test(pid)) {
+      continue;
+    }
+
+    const height = parseInt(hgt);
+    if (hgt.endsWith("cm")) {
+      if (height < 150 || 193 < height) {
+        continue;
+      }
+    } else if (hgt.endsWith("in")) {
+      if (height < 59 || 76 < height) {
+        continue;
+      }
+    } else {
+      continue;
+    }
+
+    valid += 1;
+  }
+  return valid;
+}
+
+function getPassportObject(passportData: string) {
+  return Object.fromEntries([...passportData.matchAll(pattern)]
+    .map(([, field, value]) => [field, value]));
+}
---
+++ year2020/day04_test.ts
@@ -0,0 +1,65 @@
+import { assertEquals } from "https://deno.land/std@0.108.0/testing/asserts.ts";
+import { part1, part2 } from "./day04.ts";
+
+const example1 = `ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in`;
+
+const example2 = `eyr:1972 cid:100
+hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+
+iyr:2019
+hcl:#602927 eyr:1967 hgt:170cm
+ecl:grn pid:012533040 byr:1946
+
+hcl:dab227 iyr:2012
+ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+
+hgt:59cm ecl:zzz
+eyr:2038 hcl:74454a iyr:2023
+pid:3556412378 byr:2007`;
+
+const example3 = `pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+hcl:#623a2f
+
+eyr:2029 ecl:blu cid:129 byr:1989
+iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
+
+hcl:#888785
+hgt:164cm byr:2001 iyr:2015 cid:88
+pid:545766238 ecl:hzl
+eyr:2022
+
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719`;
+
+const data = await Deno.readTextFile("year2020/day04_data.txt");
+
+Deno.test("part1(example)", () => {
+  assertEquals(part1(example1), 2);
+});
+
+Deno.test("part1(data)", () => {
+  assertEquals(part1(data), 190);
+});
+
+Deno.test("part2(example2)", () => {
+  assertEquals(part2(example2), 0);
+});
+
+Deno.test("part2(example3)", () => {
+  assertEquals(part2(example3), 4);
+});
+
+Deno.test("part2(data)", () => {
+  assertEquals(part2(data), 121);
+});
