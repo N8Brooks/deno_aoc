c0df0b65e8b04efb9405a5f8e13e86262f54f893
Nathan Brooks <natebrooks004@gmail.com>
2021-10-11 22:08:43 -0700

    year2020/day10

---
+++ year2020/day10.ts
@@ -0,0 +1,34 @@
+export function part1(data: string): number {
+  const numbers = data
+    .split("\n")
+    .map(Number)
+    .sort((a, b) => a - b);
+  numbers.push(numbers[numbers.length - 1] + 3);
+  let pre = 0, one = 0, three = 0;
+  for (const num of numbers) {
+    switch (num - pre) {
+      case 1:
+        one++;
+        break;
+      case 3:
+        three++;
+        break;
+    }
+    pre = num;
+  }
+  return one * three;
+}
+
+export function part2(data: string): number {
+  const numbers = data
+    .split("\n")
+    .map(Number)
+    .sort((a, b) => a - b);
+  numbers.push(numbers[numbers.length - 1] + 3);
+  let window = [[0, 1]];
+  for (const num of numbers) {
+    window = window.filter(([element]) => num - element <= 3);
+    window.push([num, window.reduce((sum, [, ways]) => sum + ways, 0)]);
+  }
+  return window[window.length - 1][1];
+}
---
+++ year2020/day10_test.ts
@@ -0,0 +1,72 @@
+import { assertEquals } from "https://deno.land/std@0.108.0/testing/asserts.ts";
+import { part1, part2 } from "./day10.ts";
+
+const example1 = `16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4`;
+
+const example2 = `28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3`;
+
+const data = await Deno.readTextFile("year2020/day10_data.txt");
+
+Deno.test("part1(example1)", () => {
+  assertEquals(part1(example1), 35);
+});
+
+Deno.test("part1(example2)", () => {
+  assertEquals(part1(example2), 220);
+});
+
+Deno.test("part1(example1)", () => {
+  assertEquals(part1(data), 2760);
+});
+
+Deno.test("part2(example1)", () => {
+  assertEquals(part2(example1), 8);
+});
+
+Deno.test("part2(example2)", () => {
+  assertEquals(part2(example2), 19208);
+});
+
+Deno.test("part2(example1)", () => {
+  assertEquals(part2(data), 13816758796288);
+});
