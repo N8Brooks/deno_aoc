3ce56a0ffa7242a919185d0e730927cb657169ca
Nathan Brooks <natebrooks004@gmail.com>
2021-11-06 18:01:15 -0700

    year2017/day06

---
+++ year2017/day06.ts
@@ -0,0 +1,81 @@
+const iMax = (array: number[]): [number, number] => {
+  let maxIndex = -1, maxValue = -Infinity;
+  for (let index = 0; index < array.length; index++) {
+    const value = array[index];
+    if (value > maxValue) {
+      maxValue = value;
+      maxIndex = index;
+    }
+  }
+  return [maxValue, maxIndex];
+};
+
+export function part1(data: string): number {
+  const banks = data.split("\t").map(Number);
+  const states: Set<string> = new Set();
+  let key = banks.join();
+  do {
+    states.add(key);
+    const [maxValue, maxIndex] = iMax(banks);
+    const remainder = maxValue % banks.length;
+    const quotient = Math.floor(maxValue / banks.length);
+    banks[maxIndex] = 0;
+    if (quotient > 0) {
+      for (let i = 0; i < banks.length; i++) {
+        banks[i] += quotient;
+      }
+    }
+    if (remainder > 0) {
+      const endIndex = maxIndex + remainder + 1;
+      if (endIndex > banks.length) {
+        for (let i = maxIndex + 1; i < banks.length; i++) {
+          banks[i]++;
+        }
+        for (let i = 0; i < endIndex % banks.length; i++) {
+          banks[i]++;
+        }
+      } else {
+        for (let i = maxIndex + 1; i < endIndex; i++) {
+          banks[i]++;
+        }
+      }
+    }
+    key = banks.join();
+  } while (!states.has(key));
+  return states.size;
+}
+
+export function part2(data: string): number {
+  const banks = data.split("\t").map(Number);
+  const states: Map<string, number> = new Map();
+  let key = banks.join();
+  do {
+    states.set(key, states.size);
+    const [maxValue, maxIndex] = iMax(banks);
+    const remainder = maxValue % banks.length;
+    const quotient = Math.floor(maxValue / banks.length);
+    banks[maxIndex] = 0;
+    if (quotient > 0) {
+      for (let i = 0; i < banks.length; i++) {
+        banks[i] += quotient;
+      }
+    }
+    if (remainder > 0) {
+      const endIndex = maxIndex + remainder + 1;
+      if (endIndex > banks.length) {
+        for (let i = maxIndex + 1; i < banks.length; i++) {
+          banks[i]++;
+        }
+        for (let i = 0; i < endIndex % banks.length; i++) {
+          banks[i]++;
+        }
+      } else {
+        for (let i = maxIndex + 1; i < endIndex; i++) {
+          banks[i]++;
+        }
+      }
+    }
+    key = banks.join();
+  } while (!states.has(key));
+  return states.size - (states.get(key) as number);
+}
---
+++ year2017/day06_test.ts
@@ -0,0 +1,28 @@
+import { expect } from "../test/setup.ts";
+import { part1, part2 } from "./day06.ts";
+
+const data = await Deno.readTextFile("year2017/day06_data.txt");
+
+const example = "0\t2\t7\t0";
+
+describe("day06", () => {
+  describe("part1", () => {
+    it("example1", () => {
+      expect(part1(example)).to.equal(5);
+    });
+
+    it("data", () => {
+      expect(part1(data)).to.equal(12841);
+    });
+  });
+
+  describe("part2", () => {
+    it("example", () => {
+      expect(part2(example)).to.equal(4);
+    });
+
+    it("data", () => {
+      expect(part2(data)).to.equal(8038);
+    });
+  });
+});
