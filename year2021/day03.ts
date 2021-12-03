14ccdb2e9d6c8a21336a4f41fa6abdccfa65bb53
Nathan Brooks <natebrooks004@gmail.com>
2021-12-02 22:49:25 -0800

    year2021/day03

---
+++ year2021/day03.ts
@@ -0,0 +1,60 @@
+export function part1(data: string): number {
+  const report = data.split("\n").map((line) => line.split("").map(Number));
+  let gamma = 0;
+  let epsilon = 0;
+  for (let j = 0; j < report[0].length; j++) {
+    const bitCounts = [0, 0];
+    for (let i = 0; i < report.length; i++) {
+      const bit = report[i][j];
+      bitCounts[bit]++;
+    }
+    gamma = gamma << 1 | oneMostCommon(bitCounts);
+    epsilon = epsilon << 1 | zeroMostCommon(bitCounts);
+  }
+  return gamma * epsilon;
+}
+
+export function part2(data: string): number {
+  const report = data.split("\n").map((line) => line.split("").map(Number));
+  const oxygen = criteriaCompliant(report, oneMostCommon);
+  const co2 = criteriaCompliant(report, zeroMostCommon);
+  return oxygen * co2;
+}
+
+/** Returns the number where all bits comply with the given criteria */
+function criteriaCompliant(
+  report: number[][],
+  criteria: (bitCounts: number[]) => number,
+) {
+  let j = 0;
+  while (report.length > 1) {
+    const bitCounts = [0, 0];
+    for (let i = 0; i < report.length; i++) {
+      const bit = report[i][j];
+      bitCounts[bit]++;
+    }
+    const bit = +criteria(bitCounts);
+    report = report.filter((bits) => bits[j] === bit);
+    j++;
+  }
+  return bitsToNumber(report[0]);
+}
+
+/** Returns `1` if there are equal or more `1`s than `0`s */
+function oneMostCommon([zeroBits, oneBits]: number[]): number {
+  return +(oneBits >= zeroBits);
+}
+
+/** Returns `1` if there are more `0`s than `1`s */
+function zeroMostCommon([zeroBits, oneBits]: number[]): number {
+  return +(zeroBits > oneBits);
+}
+
+/** Converts a binary `Array` to its numerical equivalent */
+function bitsToNumber(bits: number[]): number {
+  let rating = 0;
+  for (const bit of bits) {
+    rating = rating << 1 | bit;
+  }
+  return rating;
+}
---
+++ year2021/day03_test.ts
@@ -0,0 +1,39 @@
+import { expect } from "../test/setup.ts";
+import { part1, part2 } from "./day03.ts";
+
+const example = `00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010`;
+
+const data = await Deno.readTextFile("year2021/day03_data.txt");
+
+describe("day03", () => {
+  describe("part1", () => {
+    it("example", () => {
+      expect(part1(example)).to.equal(198);
+    });
+
+    it("data", () => {
+      expect(part1(data)).to.equal(3242606);
+    });
+  });
+
+  describe("part2", () => {
+    it("example", () => {
+      expect(part2(example)).to.equal(230);
+    });
+
+    it("data", () => {
+      expect(part2(data)).to.equal(4856080);
+    });
+  });
+});
