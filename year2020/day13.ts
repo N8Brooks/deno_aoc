3015faf63275450434eb05257fa85fc1d76c4562
Nathan Brooks <natebrooks004@gmail.com>
2021-12-05 15:55:19 -0800

    feat: year2020/day13

---
+++ year2020/day13.ts
@@ -0,0 +1,46 @@
+export function part1(input: string): number {
+  const [notes, schedule] = input.split("\n");
+  const timeZero = +notes;
+  let minBus = 0;
+  let minTime = Infinity;
+  schedule
+    .split(",")
+    .filter((bus) => bus !== "x")
+    .map(Number)
+    .forEach((bus) => {
+      const remainder = (bus - timeZero) % bus;
+      const time = (remainder + bus) % bus;
+      if (minTime > time) {
+        minBus = bus;
+        minTime = time;
+      }
+    });
+  return minBus * minTime;
+}
+
+const modularInverse = (n: number, m: number): number => {
+  for (let r = 1; r < m; r++) {
+    if ((n * r) % m === 1) {
+      return r;
+    }
+  }
+  return NaN;
+};
+
+export function part2(input: string): number {
+  let time = 0;
+  let lcm = 1;
+  input
+    .split("\n")[1]
+    .split(",")
+    .map((bus, i) => [parseInt(bus), i])
+    .filter(([bus]) => !isNaN(bus))
+    .forEach(([bus, i]) => {
+      const inverted = (time + i) * modularInverse(lcm, bus);
+      const quotient = bus * Math.ceil(inverted / bus) - inverted;
+      const remainder = quotient % bus;
+      time += remainder * lcm;
+      lcm *= bus;
+    });
+  return time;
+}
---
+++ year2020/day13_test.ts
@@ -0,0 +1,49 @@
+import { expect } from "../test_deps.ts";
+import { part1, part2 } from "./day13.ts";
+
+const example1 = `939
+7,13,x,x,59,x,31,19`;
+
+const input = await Deno.readTextFile("year2020/testdata/day13.txt");
+
+describe("day13", () => {
+  describe("part1", () => {
+    it("example1", () => {
+      expect(part1(example1)).to.equal(295);
+    });
+
+    it("input", () => {
+      expect(part1(input)).to.equal(4722);
+    });
+  });
+
+  describe("part2", () => {
+    it("example1", () => {
+      expect(part2(example1)).to.equal(1068781);
+    });
+
+    it("example2", () => {
+      expect(part2("-1\n17,x,13,19")).to.equal(3417);
+    });
+
+    it("example3", () => {
+      expect(part2("-1\n67,7,59,61")).to.equal(754018);
+    });
+
+    it("example4", () => {
+      expect(part2("-1\n67,x,7,59,61")).to.equal(779210);
+    });
+
+    it("example5", () => {
+      expect(part2("-1\n67,7,x,59,61")).to.equal(1261476);
+    });
+
+    it("example6", () => {
+      expect(part2("-1\n1789,37,47,1889")).to.equal(1202161486);
+    });
+
+    it("input", () => {
+      expect(part2(input)).to.equal(825305207525452);
+    });
+  });
+});
