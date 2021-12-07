6d75abc60769691fdf6ddb99f8f675613029e23a
Nathan Brooks <natebrooks004@gmail.com>
2021-12-06 21:23:54 -0800

    style: remove imports that are not used from deps

--- deps.ts
+++ deps.ts
@@ -1,4 +1,3 @@
 export { crypto } from "https://deno.land/std@0.117.0/crypto/mod.ts";
 export * from "https://deno.land/x/generator_helpers@0.0.0/monkey_patch.ts";
 export * from "https://deno.land/x/combinatorics@1.0.1/mod.ts";
-export * as c from "https://deno.land/std/collections/mod.ts";
