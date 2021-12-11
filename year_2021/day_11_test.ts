82da07f7c84c57e24c24c260c0e0f99346fe3e19
Nathan Brooks <natebrooks004@gmail.com>
2021-12-10 19:47:54 -0800

    year2021/day11

---
+++ year_2021/day_11_test.ts
@@ -0,0 +1,37 @@
+import { expect } from "../test_deps.ts";
+import { part1, part2 } from "./day_11.ts";
+
+const EXAMPLE = `5483143223
+2745854711
+5264556173
+6141336146
+6357385478
+4167524645
+2176841721
+6882881134
+4846848554
+5283751526`;
+
+const input = await Deno.readTextFile("year_2021/testdata/day_11.txt");
+
+describe("day11", () => {
+  describe("part1", () => {
+    it("example", () => {
+      expect(part1(EXAMPLE)).to.equal(1656);
+    });
+
+    it("input", () => {
+      expect(part1(input)).to.equal(1757);
+    });
+  });
+
+  describe("part2", () => {
+    it("example", () => {
+      expect(part2(EXAMPLE)).to.equal(195);
+    });
+
+    it("input", () => {
+      expect(part2(input)).to.equal(422);
+    });
+  });
+});
