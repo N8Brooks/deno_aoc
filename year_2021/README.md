25a1bb69b33bcb2fd881b46c20c49879ca19df18
Nathan Brooks <natebrooks004@gmail.com>
2021-12-13 19:29:45 -0800

    feat: year 2021 day 14

--- year_2021/README.md
+++ year_2021/README.md
@@ -1,8 +1,8 @@
 # year_2021
 
-![](https://img.shields.io/badge/day%20📅-14-blue)
-![](https://img.shields.io/badge/stars%20⭐-26-yellow)
-![](https://img.shields.io/badge/days%20completed-13-green)
+![](https://img.shields.io/badge/day%20📅-15-blue)
+![](https://img.shields.io/badge/stars%20⭐-28-yellow)
+![](https://img.shields.io/badge/days%20completed-14-green)
 
 Advent of Code solutions for year 2021. Currently in progress.
 
---
+++ year_2021/day_14_test.ts
@@ -0,0 +1,45 @@
+import { expect } from "../test_deps.ts";
+import { part1, part2 } from "./day_14.ts";
+
+const EXAMPLE = `NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C`;
+
+const input = await Deno.readTextFile("year_2021/testdata/day_14.txt");
+
+describe("day 14", () => {
+  describe("part 1", () => {
+    it("example", () => {
+      expect(part1(EXAMPLE)).to.equal(1588);
+    });
+
+    it("input", () => {
+      expect(part1(input)).to.equal(3831);
+    });
+  });
+
+  describe("part 2", () => {
+    it("example", () => {
+      expect(part2(EXAMPLE)).to.equal(2188189693529);
+    });
+
+    it("input", () => {
+      expect(part2(input)).to.equal(5725739914282);
+    });
+  });
+});
