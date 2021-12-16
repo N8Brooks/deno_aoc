535189e897bfcd0e0755765ec439040fa32b0a67
Nathan Brooks <natebrooks004@gmail.com>
2021-12-15 20:58:20 -0800

    feat: year 2019 day 2

--- year_2019/README.md
+++ year_2019/README.md
@@ -1,7 +1,7 @@
 # year_2019
 
-![](https://img.shields.io/badge/day%20📅-2-blue)
-![](https://img.shields.io/badge/stars%20⭐-4-yellow)
-![](https://img.shields.io/badge/days%20completed-2-red)
+![](https://img.shields.io/badge/day%20📅-3-blue)
+![](https://img.shields.io/badge/stars%20⭐-6-yellow)
+![](https://img.shields.io/badge/days%20completed-3-red)
 
 Advent of Code solutions for year 2019.
---
+++ year_2019/day_02_test.ts
@@ -0,0 +1,18 @@
+import { expect } from "../test_deps.ts";
+import { part1, part2 } from "./day_02.ts";
+
+const input = await Deno.readTextFile("year_2019/testdata/day_02.txt");
+
+describe("day 2", () => {
+  describe("part 1", () => {
+    it("input", () => {
+      expect(part1(input)).to.equal(3760627);
+    });
+  });
+
+  describe("part 2", () => {
+    it("input", () => {
+      expect(part2(input)).to.equal(7195);
+    });
+  });
+});
