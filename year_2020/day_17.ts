037a9a0a3a38171bbb78a042339f9637aeea2f8c
skookumchoocher <natebrooks004@gmail.com>
2021-12-17 11:45:42 -0800

    fix: year 2020 day 17
    Whoops, wrong directory

--- year_2020/day_17.ts
+++ year_2020/day_17.ts
@@ -1,58 +1,44 @@
+import { permutationsWithReplacement } from "../deps.ts";
+
+const simulate = (input: string, dimensions: number): number => {
+  const coordinatePadding = new Array(dimensions - 2).fill(0);
+  const neighbors = [...permutationsWithReplacement([-1, 1, 0], dimensions)];
+  neighbors.length -= 1;
+  let previousCubes: Set<string> = new Set();
+  input.split("\n").forEach((line, y) => {
+    line.split("").forEach((char, x) => {
+      if (char === "#") {
+        const key = [x, y, ...coordinatePadding].join();
+        previousCubes.add(key);
+      }
+    });
+  });
+  for (let cycle = 0; cycle < 6; cycle++) {
+    const counts: Map<string, number> = new Map();
+    for (const sourceKey of previousCubes) {
+      const sourceCube = sourceKey.split(",").map((d) => +d);
+      for (const neighbor of neighbors) {
+        const destCube = sourceCube.map((x, i) => x + neighbor[i]);
+        const destKey = destCube.join();
+        const count = counts.get(destKey) ?? 0;
+        counts.set(destKey, count + 1);
+      }
+    }
+    const currentCubes: Set<string> = new Set();
+    for (const [key, count] of counts) {
+      if (count === 3 || count === 2 && previousCubes.has(key)) {
+        currentCubes.add(key);
+      }
+    }
+    previousCubes = currentCubes;
+  }
+  return previousCubes.size;
+};
+
 export function part1(input: string): number {
-  const y0 = +input.match(/y=(-?\d+)/)![1];
-  return y0 * (y0 - Math.sign(y0)) / 2;
+  return simulate(input, 3);
 }
 
 export function part2(input: string): number {
-  const [x0, x1, y0, y1] = [...input.matchAll(/[\d\-]+/g)].map((d) => +d);
-  let targetReachedCount = 0;
-
-  let yVelocity = -y0;
-  let xVelocityLeftBound = Math.ceil((Math.sqrt(8 * x0) - 1) / 2);
-  let xVelocityRightBound = xVelocityLeftBound + 1;
-
-  while (y0 <= --yVelocity) {
-    // Update x velocity left bound
-    let xVelocity = xVelocityLeftBound;
-    let targetAreaReached = reachesTargetArea(xVelocity, yVelocity);
-    while (xVelocity <= x1 && !targetAreaReached) {
-      xVelocity++;
-      targetAreaReached = reachesTargetArea(xVelocity, yVelocity);
-    }
-    if (!targetAreaReached) {
-      continue;
-    }
-    xVelocityLeftBound = xVelocity;
-
-    // Update x velocity right bound
-    if (xVelocityRightBound > xVelocity) {
-      xVelocity = xVelocityRightBound;
-      targetAreaReached = reachesTargetArea(xVelocity, yVelocity);
-    }
-    while (targetAreaReached) {
-      xVelocity++;
-      targetAreaReached = reachesTargetArea(xVelocity, yVelocity);
-    }
-    xVelocityRightBound = xVelocity;
-
-    targetReachedCount += xVelocityRightBound - xVelocityLeftBound;
-  }
-
-  return targetReachedCount;
-
-  function reachesTargetArea(xVelocity0: number, yVelocity0: number): boolean {
-    let xVelocity1 = xVelocity0;
-    let yVelocity1 = yVelocity0;
-    let x = 0, y = 0;
-    do {
-      x += xVelocity1;
-      y += yVelocity1;
-      xVelocity1 -= Math.sign(xVelocity1);
-      yVelocity1--;
-      if (x0 <= x && x <= x1 && y0 <= y && y <= y1) {
-        return true;
-      }
-    } while (y >= y0 && x < x1);
-    return false;
-  }
+  return simulate(input, 4);
 }
