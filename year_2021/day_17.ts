377efa126a0481c697b3e857819a6b30ef204231
skookumchoocher <natebrooks004@gmail.com>
2021-12-17 10:54:01 -0800

    refactor: year 2021 day 17 part 2

--- year_2021/day_17.ts
+++ year_2021/day_17.ts
@@ -7,11 +7,22 @@ export function part2(input: string): number {
   const [x0, x1, y0, y1] = [...input.matchAll(/[\d\-]+/g)].map((d) => +d);
   let targetReachedCount = 0;
 
-  for (let xVelocity0 = 1; xVelocity0 < 256; xVelocity0++) {
-    for (let yVelocity0 = -256; yVelocity0 < 256; yVelocity0++) {
-      let xVelocity1 = xVelocity0;
-      let yVelocity1 = yVelocity0;
-      let x = 0, y = 0;
+  const minimumXVelocity = Math.ceil((Math.sqrt(8 * x0) - 1) / 2);
+  const maximumXVelocity = x1;
+
+  const minimumYVelocity = y0;
+  const maximumYVelocity = -1 - y0;
+
+  for (
+    let xVelocity0 = minimumXVelocity;
+    xVelocity0 <= maximumXVelocity;
+    xVelocity0++
+  ) {
+    for (
+      let yVelocity0 = minimumYVelocity;
+      yVelocity0 <= maximumYVelocity;
+      yVelocity0++
+    ) {
       do {
         x += xVelocity1;
         y += yVelocity1;
