b08c1a8b1c724d4c6e1b7758ba0c5416b3bffbb8
skookumchoocher <natebrooks004@gmail.com>
2021-12-20 11:18:20 -0800

    refactor: remove dependencies that aren't used

--- deps.ts
+++ deps.ts
@@ -4,6 +4,5 @@ export {
   slidingWindows,
   zip,
 } from "https://deno.land/std@0.117.0/collections/mod.ts";
-export { BinaryHeap } from "https://deno.land/x/collections@0.11.2/binary_heap.ts";
 export * from "https://deno.land/x/generator_helpers@0.0.0/monkey_patch.ts";
 export * from "https://deno.land/x/combinatorics@1.0.1/mod.ts";
