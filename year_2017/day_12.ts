6c20fdcdaf11c20464dbb79723cf614a1de60095
skookumchoocher <natebrooks004@gmail.com>
2021-12-31 12:09:44 -0800

    feat: year 2017 day 12

---
+++ year_2017/day_12.ts
@@ -0,0 +1,31 @@
+const groupsFactory = (input: string): number[] => {
+  const groups: number[] = [];
+  const find = (x: number): number => {
+    const groupX = groups[x] ??= x;
+    return groupX === x ? x : find(groupX);
+  };
+  for (const line of input.split("\n")) {
+    const [a, bs] = line.split(" <-> ");
+    const groupA = find(+a);
+    for (const b of bs.split(", ")) {
+      const groupB = find(+b);
+      groups[groupB] = groupA;
+    }
+  }
+  return groups.map((x) => find(x));
+};
+
+export function part1(input: string): number {
+  const groups = groupsFactory(input);
+  let group0Count = 0;
+  for (const group of groups) {
+    if (group === groups[0]) {
+      group0Count++;
+    }
+  }
+  return group0Count;
+}
+
+export function part2(input: string): number {
+  return new Set(groupsFactory(input)).size;
+}
---
+++ year_2017/day_12_test.ts
@@ -0,0 +1,34 @@
+import { expect } from "../test_deps.ts";
+import { part1, part2 } from "./day_12.ts";
+
+const input = await Deno.readTextFile("year_2017/testdata/day_12.txt");
+
+const EXAMPLE = `0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5`;
+
+describe("day 12", () => {
+  describe("part 1", () => {
+    it("example", () => {
+      expect(part1(EXAMPLE)).to.equal(6);
+    });
+
+    it("input", () => {
+      expect(part1(input)).to.equal(306);
+    });
+  });
+
+  describe("part 2", () => {
+    it("example", () => {
+      expect(part2(EXAMPLE)).to.equal(2);
+    });
+
+    it("input", () => {
+      expect(part2(input)).to.equal(200);
+    });
+  });
+});
