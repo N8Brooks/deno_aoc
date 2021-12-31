c604fa5e19895301d0c20774984538bce94fdc24
skookumchoocher <natebrooks004@gmail.com>
2021-12-31 11:32:39 -0800

    feat: year 2017 day 11

---
+++ year_2017/day_11.ts
@@ -0,0 +1,31 @@
+/** Maps direction to it hexagonal sign */
+const COMPLEX_DIRECTIONS: Record<string, number[]> = {
+  nw: [1, 0],
+  n: [0, 1],
+  ne: [-1, 1],
+  se: [-1, 0],
+  s: [0, -1],
+  sw: [1, -1],
+};
+
+/** Distance from origin for a hex coordinate */
+const distance = ([real, imag]: number[]): number => {
+  return (Math.abs(imag) + Math.abs(real + imag) + Math.abs(real)) / 2;
+};
+
+export function part1(input: string): number {
+  const destination = input
+    .split(",")
+    .map((direction) => COMPLEX_DIRECTIONS[direction])
+    .reduce((a, b) => [a[0] + b[0], a[1] + b[1]]);
+  return distance(destination);
+}
+
+export function part2(input: string): number {
+  return input
+    .split(",")
+    .map((direction) => COMPLEX_DIRECTIONS[direction])
+    .map(((a) => (b) => [a[0] += b[0], a[1] += b[1]])([0, 0]))
+    .map((position) => distance(position))
+    .reduce((a, b) => a > b ? a : b);
+}
---
+++ year_2017/day_11_test.ts
@@ -0,0 +1,50 @@
+import { expect } from "../test_deps.ts";
+import { part1, part2 } from "./day_11.ts";
+
+const input = await Deno.readTextFile("year_2017/testdata/day_11.txt");
+
+describe("day 11", () => {
+  describe("part 1", () => {
+    it("example 1", () => {
+      expect(part1("ne,ne,ne")).to.equal(3);
+    });
+
+    it("example 2", () => {
+      expect(part1("ne,ne,sw,sw")).to.equal(0);
+    });
+
+    it("example 3", () => {
+      expect(part1("ne,ne,s,s")).to.equal(2);
+    });
+
+    it("example 4", () => {
+      expect(part1("se,sw,se,sw,sw")).to.equal(3);
+    });
+
+    it("input", () => {
+      expect(part1(input)).to.equal(784);
+    });
+  });
+
+  describe("part 2", () => {
+    it("example 1", () => {
+      expect(part2("ne,ne,ne")).to.equal(3);
+    });
+
+    it("example 2", () => {
+      expect(part2("ne,ne,sw,sw")).to.equal(2);
+    });
+
+    it("example 3", () => {
+      expect(part2("ne,ne,s,s")).to.equal(2);
+    });
+
+    it("example 4", () => {
+      expect(part2("se,sw,se,sw,sw")).to.equal(3);
+    });
+
+    it("input", () => {
+      expect(part2(input)).to.equal(1558);
+    });
+  });
+});
