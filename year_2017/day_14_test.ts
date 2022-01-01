fde695c43d6a2d2382140f7b3d0f48c99a0f1ae7
Nathan Brooks <natebrooks004@gmail.com>
2021-12-31 22:49:19 -0800

    feat: year 2017 day 14

--- year_2017/day_14_test.ts
+++ year_2017/day_14_test.ts
@@ -5,24 +5,24 @@ const input = await Deno.readTextFile("year_2017/testdata/day_14.txt");
 
 const EXAMPLE = "flqrgnkx";
 
-// describe("day 14", () => {
-//   describe("part 1", () => {
-//     it("example", () => {
-//       expect(part1(EXAMPLE)).to.equal(8108);
-//     });
+describe("day 14", () => {
+  describe("part 1", () => {
+    it("example", () => {
+      expect(part1(EXAMPLE)).to.equal(8108);
+    });
 
-//     it("input", () => {
-//       expect(part1(input)).to.equal(2604);
-//     });
-//   });
+    it("input", () => {
+      expect(part1(input)).to.equal(8106);
+    });
+  });
 
-//   describe("part 2", () => {
-//     it("example", () => {
-//       expect(part2(EXAMPLE)).to.equal(10);
-//     });
+  describe("part 2", () => {
+    it("example", () => {
+      expect(part2(EXAMPLE)).to.equal(1242);
+    });
 
-//     it("input", () => {
-//       expect(part2(input)).to.equal(3941460);
-//     });
-//   });
-// });
+    it("input", () => {
+      expect(part2(input)).to.equal(1164);
+    });
+  });
+});
